ba62633157cd7c573df79a7765af1be6dbd4c646
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2023-06-09 16:34:03 +0530

    icon change

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+// import Alert from './components/Alert';
+// import { useState } from 'react';
 
 function App() {
+  // const[alert,setAlert]=useState(null)
+  // const serAlert=(message,type)=>{
+  //   setAlert({
+  //     msg:message,
+     
+  //   })
+  // }
   return (
    <>
+   
   <Navbar title="TextUtlis" about="About TextUtlis"/>
+  {/* <Alert alert="this is alert"/> */}
   <div className="container">
   <TextForm/>
   
   <Footer/>
 
   </div>
+  
   
   
    </>
