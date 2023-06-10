3b033611009381a7478e4b71a4380fed06e2c3a3
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2023-06-10 12:22:48 +0530

    routes problem

--- src/App.js
+++ src/App.js
@@ -5,28 +5,23 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 
+
+
 function App() {
-  // const[alert,setAlert]=useState(null)
-  // const serAlert=(message,type)=>{
-  //   setAlert({
-  //     msg:message,
-     
-  //   })
-  // }
+
   return (
    <>
+   
   <Navbar title="TextUtlis" about="About TextUtlis"/>
   <div className="container">
-  {/* <TextForm/> */}
-  <Routes>
-  <Route path="/" element={<TextForm />} />
-  <Route path="/About.js" element={<About />} />
+  <TextForm/>
+  
   {/* <About/> */}
   
   
-  </Routes>
+ 
   </div>
-  
+  <Footer/>
   
    </>
   );
