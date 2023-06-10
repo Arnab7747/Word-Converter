5731a5b1106194e20c7962d3bcf56abf9b164741
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2023-06-10 12:13:09 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
+import About from './components/About';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
    <>
-  <Navbar title="TextUtlis" about="About TextUtlis"/>
-  <div className="container">
-  <TextForm/>
+   <Router>
   
-  <Footer/>
-
+  <Navbar title="TextUtlis" about="About TextUtlis"/>
+  
+  <div className="container">
+  {/* <TextForm/> */}
+  <Routes>
+  <Route path="/" element={<TextForm />} />
+  <Route path="/About.js" element={<About />} />
+  {/* <About/> */}
+  
+  
+  </Routes>
   </div>
+  
+  </Router>
+  <Footer/>
   
   
    </>
